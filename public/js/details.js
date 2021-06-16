64d1e60d5b9db22b1ac4b7dce5cd4584d26b76df
Booker Washington <bwashington@salesforce.com>
2021-06-15 23:24:23 -0500

    click path update

--- public/js/details.js
+++ public/js/details.js
@@ -1,2 +1,27 @@
 const queryString = window.location.search;
 console.log(queryString);
+
+let i = 0;
+
+setTimeout(function(){ 
+  $('#exampleModalCenter').modal('show'); 
+ }, 10000);
+
+$(document).mouseleave(function () {
+  if(i < 1)
+    $('#exampleModalCenter').modal('show'); 
+
+  i++;
+});
+
+$(".sign-up").click(function(){
+  $(".get-recommendations").html(`
+    <h3 class="v-space-25">Thanks for signing up!</h3>
+    <p>
+      Look for home office inspiration in your inbox shortly.
+      We'll send style advice, tips and tricks, and other info to
+      keep you inspired and creating the functional space you love. 
+      You'll be working in style in no time.
+    </p>
+  `)
+})
