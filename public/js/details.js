fac062a9095a3ed0012cf04b2aff88a367dc22f6
redcoatmedia <gina.nguyen@ginanguyen-ltm.internal.salesforce.com>
2021-06-17 20:59:38 -0400

    added hidden variable for email address

--- public/js/details.js
+++ public/js/details.js
@@ -3,24 +3,25 @@ console.log(queryString);
 
 let i = 0;
 
-setTimeout(function(){ 
-  $('#exampleModalCenter').modal('show'); 
+setTimeout(function(){
+  $('#exampleModalCenter').modal('show');
  }, 10000);
 
 $(document).mouseleave(function () {
   if(i < 1)
-    $('#exampleModalCenter').modal('show'); 
+    $('#exampleModalCenter').modal('show');
 
   i++;
 });
 
 $(".sign-up").click(function(){
+  $("#hiddenEmail").val($("#newsletterEmail").val());
   $(".get-recommendations").html(`
     <h3 class="v-space-25">Thanks for signing up!</h3>
     <p>
       Look for home office inspiration in your inbox shortly.
       We'll send style advice, tips and tricks, and other info to
-      keep you inspired and creating the functional space you love. 
+      keep you inspired and creating the functional space you love.
       You'll be working in style in no time.
     </p>
   `)
